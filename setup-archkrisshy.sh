d5d81a5de0587a3fc4abb8dab24143eb61d24f66
serverkrisshy <160938936+serverkrisshy@users.noreply.github.com>
2024-02-23 19:35:06 +0700

    Update setup-archkrisshy.sh

--- setup-archkrisshy.sh
+++ setup-archkrisshy.sh
@@ -1,65 +1,30 @@
 #!/bin/sh
 
-echo -ne "
----------------------------------------------------------------------------------------
-   █████╗ ██████╗  ██████╗██╗  ██╗██╗  ██╗██████╗ ██╗███████╗███████╗██╗  ██╗██╗   ██╗
-  ██╔══██╗██╔══██╗██╔════╝██║  ██║██║ ██╔╝██╔══██╗██║██╔════╝██╔════╝██║  ██║╚██╗ ██╔╝
-  ███████║██████╔╝██║     ███████║█████╔╝ ██████╔╝██║███████╗███████╗███████║ ╚████╔╝ 
-  ██╔══██║██╔══██╗██║     ██╔══██║██╔═██╗ ██╔══██╗██║╚════██║╚════██║██╔══██║  ╚██╔╝  
-  ██║  ██║██║  ██║╚██████╗██║  ██║██║  ██╗██║  ██║██║███████║███████║██║  ██║   ██║   
-  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝                                                                                         
----------------------------------------------------------------------------------------
-                            Automated Arch Linux Installer
----------------------------------------------------------------------------------------
-                Warning!!! This script for dual boot with Windows only
-"
+# Checking if running in Repo Folder
+if [[ "$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]')" =~ ^scripts$ ]]; then
+    echo "You are running this in ArchKrisshy Folder."
+    echo "Please use ./archkrisshy.sh instead"
+    exit
+fi
 
-#Set Keyboard Layout
-keymap () {
-echo -ne "
-Please select key board layout from this list"
-# These are default key maps as presented in official arch repo archinstall
-options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
-
-select_option $? 4 "${options[@]}"
-keymap=${options[$?]}
-
-echo -ne "Your key boards layout: ${keymap} \n"
-set_option KEYMAP $keymap
-}
-
-
-#Set Timezone
-timezone () {
-# Added this from arch wiki https://wiki.archlinux.org/title/System_time
-time_zone="$(curl --fail https://ipapi.co/timezone)"
-ip_address="$(curl ipinfo.io)"
-echo -ne "
-System detected your timezone & public IP address to be '$time_zone $ip_address' \n"
-echo -ne "Is this correct?
-" 
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-    echo "${time_zone} set as timezone"
-    set_option TIMEZONE $time_zone;;
-    n|N|no|NO|No)
-    echo "Please enter your desired timezone e.g. Asia/Jakarta :" 
-    read new_timezone
-    echo "${new_timezone} set as timezone"
-    set_option TIMEZONE $new_timezone;;
-    *) echo "Wrong option. Try again";timezone;;
-esac
-}
-
+timedatectl set-ntp true
+cd $HOME
+clear
+echo "...."
+echo "Here we Go...."
 echo "Getting the mirrorlist (Indonesian Users only) from Server Krisshy..."
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -o /etc/pacman.d/mirrorlist https://serverkrisshy.github.io/mirrorlist
-
+echo "...."
 echo "Updating keyring for optimize installation..."
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy archlinux-keyring pacman-mirrorlist git --needed --noconfirm
-timedatectl set-ntp true
+#echo "Cloning The ArchKrisshy to this Machine"
+#git clone https://github.com/serverkrisshy/ArchKrisshy.git
+clear
+echo "Please setting your Partitions first!!!"
+echo "Use fdisk /dev/your_disk_partitions or cfdisk /dev/your_disk_partition "
+fidisk -l
+#cd $HOME/ArchKrisshy
+#exec ./archkrisshy.sh
